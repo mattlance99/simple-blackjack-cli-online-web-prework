49296e7aa9756a3a235c9ca9446c77d1975d5f0a
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-15 14:35:52 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,13 +1,13 @@
 require_relative "blackjack.rb"
 card_total = 0 
 runner
-  until card_total > 21 
+  #until card_total > 21 
     welcome
     puts "hello!!!!"
     initial_round
     hit?
     current_card_total = display_card_totals
     card_total = current_card_total + card_total
-  end
+  #end
   end_game
 end
